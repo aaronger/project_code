40bb09eef09aeb53cd7ba90db3dfbe41ae610ee2
Aaron Gerding <agerding@umass.edu>
2021-11-28 18:37:30 -0500

    run prop and propbord evals

--- code/plots.R
+++ code/plots.R
@@ -1,7 +1,7 @@
 
 dat <- bind_rows(
-  evals_AR3,
-  evals_AR3_bord,
+  evals_AR3casesProp,
+  evals_AR3casesPropBord,
   .id = "graph"
 ) %>% 
   group_by(forecast_date, ahead, graph) %>% summarise(wis = mean(wis))
